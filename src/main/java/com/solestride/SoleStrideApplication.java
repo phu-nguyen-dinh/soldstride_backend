6b0d480071f4b4141729fe19b7c0615f932b9655
phunguyendinh <phu0981838248@gmail.com>
2026-02-15 23:50:54 +0700

    first commit

---
+++ src/main/java/com/solestride/SoleStrideApplication.java
@@ -0,0 +1,12 @@
+package com.solestride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoleStrideApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(SoleStrideApplication.class, args);
+    }
+}
